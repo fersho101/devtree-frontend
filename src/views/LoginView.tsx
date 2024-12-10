1323a100e58fef9bd2d12b01f4e3bf6503f1d0e1
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-12-10 11:47:57 -0700

    .

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
     return (
         <>
-            <div>LoginView</div>
+            <div className="bg-slate-50 ">LoginView</div>
             <nav>
                 <Link to='/auth/register'>
                     ¿No tinenes cuenta? .... Crea una aquí!!
